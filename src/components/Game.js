43b52aee0e914dd2b18eb613b238a19de8cfc8f1
Riot Xana <zrallwein@gmail.com>
2022-02-09 06:28:53 -0600

    pieces snap back in place now

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,24 +2,18 @@ import BoardSquare from "./BoardSquare.js";
 import React, { useState, useRef, useEffect } from "react";
 import ChessGame from "../chessUtils/chessGame.js";
 
-//everything has to be responsive. .064 is the basic vw size unit used for scaling chessboard squares
 const game = new ChessGame();
-// chess board component
 function Game() {
-  //game that handles board state data
-
   let chessBoardRef = useRef(null);
+  let activePiece = null;
+  let activePiecePosOldX = null;
+  let activePiecePosOldY = null;
 
-  //useStates
   const [chessBoardSquareSize, setChessBoardSquareSize] = useState(
     0.064 * window.innerWidth
   );
   const [boardState, setBoardState] = useState(game.boardState);
-  //haven't used it yet, but bound to need it at some point
-  const [activePiece, setActivePiece] = useState(null);
-  const [turn, setTurn] = useState(game.turn);
 
-  //when window changes sizes, so does the defined chessboardsquare size so movement still works
   const resize = () => setChessBoardSquareSize(0.064 * window.innerWidth);
   useEffect(() => {
     window.addEventListener("resize", resize);
@@ -27,33 +21,30 @@ function Game() {
     return () => window.removeEventListener("resize", resize);
   });
 
-  //function to grab onto piece and assign active piece element
   function grabPiece(e) {
     let element = e.target;
-
-    //figure out the color by slicing and dicing the css
-    const color = element.style.backgroundImage.slice(24, -6).substring(0, 5);
 
     if (
       element.classList.contains("chess-piece") &&
       !activePiece &&
-      color === turn
+      //slice up the css to find the color of the piece and compare it to game.turn
+      element.style.backgroundImage.slice(24, -6).substring(0, 5) === game.turn
     ) {
-      element.style.position = "absolute";
+      activePiece = element;
+      activePiece.style.position = "absolute";
+      activePiecePosOldX = activePiece.style.left;
+      activePiecePosOldY = activePiece.style.top;
+      console.log(activePiecePosOldX);
 
       //elements should be grabbed at the center, this takes care of that offset
-      element.style.left = `${e.clientX - chessBoardSquareSize / 2}px`;
-      element.style.top = `${e.clientY - chessBoardSquareSize / 2}px`;
-
-      setActivePiece(element);
+      activePiece.style.left = `${e.clientX - chessBoardSquareSize / 2}px`;
+      activePiece.style.top = `${e.clientY - chessBoardSquareSize / 2}px`;
     }
   }
 
-  //once active piece has been assigned, the piece can be moved
   function movePiece(e) {
     let chessBoard = chessBoardRef.current;
 
-    // lots of math to determine pixel positions, ok wel it's not that much but it is enough
     const isInBounds = (x, y) => {
       let edgeAdjust = chessBoardSquareSize / 2;
 
@@ -67,41 +58,36 @@ function Game() {
     };
 
     if (activePiece) {
-      //checks if it is inbounds, else reutnrs
       if (!isInBounds(e.clientX, e.clientY)) return;
       activePiece.style.top = `${e.clientY - chessBoardSquareSize / 2}px`;
       activePiece.style.left = `${e.clientX - chessBoardSquareSize / 2}px`;
     }
   }
 
-  //once the user is done moving the piece, it needs to be set down, and replace a piece that was there.
   function setDownPiece(e) {
     let chessBoard = chessBoardRef.current;
 
     if (activePiece) {
-      //figure out where it currently is and what type of piece it is
       let previousLocation = parseInt(activePiece.id);
-      //reset active piece so it can be used again and will stop moving
-      setActivePiece(null);
-
-      //uses pixel calculations to figure out index of where piece was dropped
       let nextLocation =
         Math.floor((e.clientX - chessBoard.offsetLeft) / chessBoardSquareSize) +
         Math.floor((e.clientY - chessBoard.offsetTop) / chessBoardSquareSize) *
           8;
 
-      //moving pieces!!!
-
-      //if there was no movement don't do anything
-      if (previousLocation === nextLocation) return;
+      if (previousLocation === nextLocation) {
+        activePiece.style.left = activePiecePosOldX;
+        activePiece.style.top = activePiecePosOldY;
+        activePiece = null;
+        return;
+      }
 
       game.movePiece(previousLocation, nextLocation);
 
-      //umm yeah it works though
       setBoardState(game.boardState.map((piece) => piece));
 
-      //now we need to change who's turn it is via inverting it with ternary operations
-      setTurn(turn === "white" ? "black" : "white");
+      game.turn = game.turn === "white" ? "black" : "white";
+
+      activePiece = null;
     }
   }
 
